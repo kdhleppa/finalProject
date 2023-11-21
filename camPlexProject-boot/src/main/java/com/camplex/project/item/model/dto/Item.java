6386843ed0537c12633e338af7a56d2a87870eed
kdhleppa <newkdh8624@gmail.com>
2023-11-21 15:21:15 +0900

    202311210321

---
+++ camPlexProject-boot/src/main/java/com/camplex/project/item/model/dto/Item.java
@@ -0,0 +1,30 @@
+package com.camplex.project.item.model.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+public class Item {
+	
+	
+	private int itemNo;
+	private String itemType;
+	private int inStoreCount;
+	private int loanCount;
+	private int itemPrice;
+	private String itemName;
+	private String itemDescription;
+	private String itemStatus;
+	private int discountRate;
+	
+	private String thumbnail;
+	
+	private List<ItemImg> imageList;
+}
