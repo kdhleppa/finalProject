b2b1e5b152dcb7fd2e3b873a982c9e2e4d12e2ba
jakelee0424 <qkql006@gmail.com>
2023-12-01 17:36:40 +0900

    231201 jaekyung

--- camPlexProject-boot/src/main/resources/static/js/qna/qnaList.js
+++ camPlexProject-boot/src/main/resources/static/js/qna/qnaList.js
@@ -18,66 +18,17 @@ const QNAAnswer = document.getElementById("QNAAnswer");
 	return;
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var list = /*[[${list}]]*/ []; // Thymeleaf를 통해 백엔드에서 가져온 리스트
-    var currentIndex = 4; // 초기로 4개의 항목을 표시
 
-    function showItems(startIndex, endIndex) {
-        var qnaContainer = document.getElementById('formContainer');
-        for (var i = startIndex; i < endIndex && i < list.length; i++) {
-            var qna = list[i];
+const divListContainer = document.getElementById('divListContainer')
+const formContainer = document.getElementById('formContainer')
+const divList = document.getElementsByClassName("divList");
 
-            // 여기에 각 항목을 표시하는 코드 작성 (기존 Thymeleaf 코드 그대로 사용)
+document.getElementById('loadMoreBtn').addEventListener('click', () => {
+    
+    console.log(divListContainer.offsetHeight)
+    const temp = divList[0].offsetHeight;
 
-            // 예시: 문의번호, 닉네임, 제목 표시
-            var qnaListHead = document.createElement('div');
-            qnaListHead.className = 'qnaListHead';
+    formContainer.style.height = divListContainer.offsetHeight + temp + 80+ "px";
+    divListContainer.style.height = divListContainer.offsetHeight + temp + 80 +"px";
 
-            var qnaNo = document.createElement('div');
-            qnaNo.className = 'qnaNo';
-            qnaNo.innerHTML = '<p id="QNANo" name="QNANo" class="form-control">' + qna.QNANo + '</p>';
-            qnaListHead.appendChild(qnaNo);
-
-            var memberNickname = document.createElement('div');
-            memberNickname.className = 'memberNickname';
-            memberNickname.innerHTML = '<p id="memberNickname" name="memberNickname" class="form-control">' + qna.memberNickname + '</p>';
-            qnaListHead.appendChild(memberNickname);
-
-            var qnaTitle = document.createElement('div');
-            qnaTitle.className = 'qnaTitle';
-            qnaTitle.innerHTML = '<p id="QNATitle" name="QNATitle" class="form-control">' + qna.QNATitle + '</p>';
-            qnaListHead.appendChild(qnaTitle);
-
-            qnaContainer.appendChild(qnaListHead);
-
-            // 여기에 내용과 답변 부분 표시하는 코드 작성 (기존 Thymeleaf 코드 그대로 사용)
-
-            // 예시: 내용 표시
-            var qnaContent = document.createElement('div');
-            qnaContent.className = 'qnaContent';
-            qnaContent.innerHTML = '<p id="QNAContent" name="QNAContent" class="form-control">' + qna.QNAContent + '</p>';
-            qnaContainer.appendChild(qnaContent);
-
-            // 예시: 답변 폼 표시
-            var qnaAnswerForm = document.createElement('form');
-            qnaAnswerForm.className = 'qnaAnswer';
-            // ... (답변 폼 관련 코드 작성, Thymeleaf 코드 그대로 사용)
-            qnaContainer.appendChild(qnaAnswerForm);
-        }
-    }
-
-    function loadMoreItems() {
-        var endIndex = currentIndex + 4;
-        if (endIndex > list.length) {
-            endIndex = list.length;
-        }
-        showItems(currentIndex, endIndex);
-        currentIndex = endIndex;
-    }
-
-    // 초기 4개 항목 표시
-    showItems(0, currentIndex);
-
-    // 더보기 버튼 클릭 시 추가 항목 로드
-    document.getElementById('loadMoreBtn').addEventListener('click', loadMoreItems);
 });
