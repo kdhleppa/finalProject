8ab24ba43cb39c778bd013e4abef4d2a01afcf59
donghyun kim <newkdh8624@gmail.com>
2023-12-13 17:36:52 +0900

    hi121

--- camPlexProject-boot/src/main/resources/static/js/map/mapModal.js
+++ camPlexProject-boot/src/main/resources/static/js/map/mapModal.js
@@ -23,6 +23,7 @@ var map = new kakao.maps.Map(mapContainerDiv, mapOption);
 // 모달 창 띄우기 함수
 function mapModal() {
 	mapContainer.classList.remove('hidden');
+	relayout()
 }
 
 function resizeMap() {
@@ -34,8 +35,5 @@ function resizeMap() {
 
 function relayout() {    
     
-    // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
-    // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다 
-    // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
     map.relayout();
 }
