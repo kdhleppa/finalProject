7bf027d5dc7f269dcde04735aefaa5831e45967b
donghyun kim <newkdh8624@gmail.com>
2023-12-16 12:32:47 +0900

    loginfilterplus

---
+++ camPlexProject-boot/src/main/java/com/camplex/project/common/filter/LoginFilter.java
@@ -0,0 +1,33 @@
+package com.camplex.project.common.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginFilter implements Filter{
+	
+
+
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		
+		HttpSession session = req.getSession();
+		
+		if( session.getAttribute("loginMember") == null ) {
+			resp.sendRedirect("/loginError");
+			
+		} else {
+			chain.doFilter(request, response);
+		}		
+	}}
