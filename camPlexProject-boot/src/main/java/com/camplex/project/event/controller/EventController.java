fd2e26a9fd8b57604ae74d582cefb849ae58d51a
jakelee0424 <qkql006@gmail.com>
2023-12-19 14:53:15 +0900

    231219 jaekyung

--- camPlexProject-boot/src/main/java/com/camplex/project/event/controller/EventController.java
+++ camPlexProject-boot/src/main/java/com/camplex/project/event/controller/EventController.java
@@ -15,25 +15,25 @@ public class EventController {
 	// 이벤트 목록 조회
 	@GetMapping("/eventList") 
 	public String eventList() {
-		return"/event/eventList";
+		return"event/eventList";
 	}
 	
 	// 이벤트 쿠폰 페이지
 	@GetMapping("/eventCoupon") 
 	public String eventCoupon() {
-		return"/event/eventCoupon";
+		return"event/eventCoupon";
 	}
 	
 	// 이벤트 룰렛 페이지
 	@GetMapping("/eventRoulette") 
 	public String eventRoulette() {
-		return"/event/eventRoulette";
+		return"event/eventRoulette";
 	}
 	
 	// 이벤트 뽑기 페이지
 	@GetMapping("/eventScratch") 
 	public String eventScratch() {
-		return"/event/eventScratch";
+		return"event/eventScratch";
 	}
 	
 }
